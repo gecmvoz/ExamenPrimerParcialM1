450e145ea49a4f308aa91d385010298fc5816e7a
GCarrion <52300475+gecmvoz@users.noreply.github.com>
2021-09-28 10:35:03 -0600

    Add files via upload

---
+++ ExamenIP1M1/Domain/ActivoFijo.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enum;
+using System;
+
+namespace Domain
+{
+    public class ActivoFijo
+    {
+        public int Id { get; set;}
+        public string CodigoActivo { get; set; }
+        public string NombreActivo { get; set; }
+        public string Descripcion { get; set;  }
+        public decimal ValorActivo { get; set; }
+        public DateTime FechaAdquisicion { get; set;  }
+        public TipoActivoFijo TipoActivoFijo { get; set; }
+        public MetodoDepreciacion MetodoDepreciacion { get; set; }
+    }
+}
---
+++ ExamenIP1M1/ExamenIP1M1/FrmActivoFijo.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace ExamenIP1M1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbCodigo = new System.Windows.Forms.Label();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbDescripcion = new System.Windows.Forms.Label();
+            this.lbValor = new System.Windows.Forms.Label();
+            this.lbFechaAdquisicion = new System.Windows.Forms.Label();
+            this.lbTipoActivo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.dtpFechaAdquisicion = new System.Windows.Forms.DateTimePicker();
+            this.cmbTipoActivo = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbTipoActivo);
+            this.groupBox1.Controls.Add(this.dtpFechaAdquisicion);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.lbTipoActivo);
+            this.groupBox1.Controls.Add(this.lbFechaAdquisicion);
+            this.groupBox1.Controls.Add(this.lbValor);
+            this.groupBox1.Controls.Add(this.lbDescripcion);
+            this.groupBox1.Controls.Add(this.lbNombre);
+            this.groupBox1.Controls.Add(this.lbCodigo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(478, 250);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Activo Fijo";
+            // 
+            // lbCodigo
+            // 
+            this.lbCodigo.AutoSize = true;
+            this.lbCodigo.Location = new System.Drawing.Point(15, 33);
+            this.lbCodigo.Name = "lbCodigo";
+            this.lbCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lbCodigo.TabIndex = 0;
+            this.lbCodigo.Text = "Codigo";
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(15, 64);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(44, 13);
+            this.lbNombre.TabIndex = 1;
+            this.lbNombre.Text = "Nombre";
+            // 
+            // lbDescripcion
+            // 
+            this.lbDescripcion.AutoSize = true;
+            this.lbDescripcion.Location = new System.Drawing.Point(15, 93);
+            this.lbDescripcion.Name = "lbDescripcion";
+            this.lbDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lbDescripcion.TabIndex = 2;
+            this.lbDescripcion.Text = "Descripcion";
+            // 
+            // lbValor
+            // 
+            this.lbValor.AutoSize = true;
+            this.lbValor.Location = new System.Drawing.Point(24, 160);
+            this.lbValor.Name = "lbValor";
+            this.lbValor.Size = new System.Drawing.Size(31, 13);
+            this.lbValor.TabIndex = 3;
+            this.lbValor.Text = "Valor";
+            // 
+            // lbFechaAdquisicion
+            // 
+            this.lbFechaAdquisicion.AutoSize = true;
+            this.lbFechaAdquisicion.Location = new System.Drawing.Point(6, 186);
+            this.lbFechaAdquisicion.Name = "lbFechaAdquisicion";
+            this.lbFechaAdquisicion.Size = new System.Drawing.Size(94, 13);
+            this.lbFechaAdquisicion.TabIndex = 4;
+            this.lbFechaAdquisicion.Text = "Fecha Adquisicion";
+            // 
+            // lbTipoActivo
+            // 
+            this.lbTipoActivo.AutoSize = true;
+            this.lbTipoActivo.Location = new System.Drawing.Point(6, 216);
+            this.lbTipoActivo.Name = "lbTipoActivo";
+            this.lbTipoActivo.Size = new System.Drawing.Size(61, 13);
+            this.lbTipoActivo.TabIndex = 5;
+            this.lbTipoActivo.Text = "Tipo Activo";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(212, 25);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(257, 20);
+            this.txtCodigo.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(212, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(257, 20);
+            this.txtNombre.TabIndex = 7;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(212, 90);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(257, 45);
+            this.txtDescripcion.TabIndex = 8;
+            // 
+            // dtpFechaAdquisicion
+            // 
+            this.dtpFechaAdquisicion.Location = new System.Drawing.Point(212, 186);
+            this.dtpFechaAdquisicion.Name = "dtpFechaAdquisicion";
+            this.dtpFechaAdquisicion.Size = new System.Drawing.Size(257, 20);
+            this.dtpFechaAdquisicion.TabIndex = 9;
+            // 
+            // cmbTipoActivo
+            // 
+            this.cmbTipoActivo.FormattingEnabled = true;
+            this.cmbTipoActivo.Location = new System.Drawing.Point(212, 216);
+            this.cmbTipoActivo.Name = "cmbTipoActivo";
+            this.cmbTipoActivo.Size = new System.Drawing.Size(257, 21);
+            this.cmbTipoActivo.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 351);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbTipoActivo;
+        private System.Windows.Forms.Label lbFechaAdquisicion;
+        private System.Windows.Forms.Label lbValor;
+        private System.Windows.Forms.Label lbDescripcion;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbCodigo;
+        private System.Windows.Forms.ComboBox cmbTipoActivo;
+        private System.Windows.Forms.DateTimePicker dtpFechaAdquisicion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+    }
+}
+
